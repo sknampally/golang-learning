81297d424c631dd09197dff4d7c517b515c732fb
pnaveenreddy26 <pnaveen_reddy@yahoo.com>
2020-04-21 09:21:30 +0530

    golang-basics

---
+++ basics/first.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("welcome to go lang ")
+	//Calling Values Display
+	valDisp()
+	//Calling Variable Declaration
+	varDisp();
+	//Calling Constant Values
+	constDisp();
+	//Calling Math Function
+	mathDisp();
+	//calling if condition
+	valid();
+	//calling multi if condition
+	multidisp();
+	//calling eny variable
+	envdisp();
+}
